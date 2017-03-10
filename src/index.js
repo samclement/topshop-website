4a5527c32e617a7452522f1aa2109c7be1202676
Sam Clement <sam@swhurl.com>
2017-03-10 19:47:14 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React, { Component  } from 'react';
+import { render } from 'react-dom'
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+
+  render() {
+    return (
+      <h1>{ `Test App 4` }</h1>
+    )
+  }
+}
+
+render(
+  <App/>,
+  document.getElementById('app')
+)
---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    './src/index.js',
+    'webpack-hot-middleware/client?reload=true'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: 'babel-loader'}
+    ]
+  }
+};
