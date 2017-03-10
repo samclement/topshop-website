86c3e8bec60fc40228508e987b5ad5f1ed265fdb
Sam Clement <sam@swhurl.com>
2017-03-10 23:15:48 +0000

    UPDATE prettier formatting

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-import React, { Component  } from 'react';
-import { render } from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
 class App extends Component {
   constructor(props, context) {
-    super(props, context)
+    super(props, context);
   }
 
   render() {
-    return (
-      <h1>{ `Test App 4` }</h1>
-    )
+    return <h1>{`Test App 4`}</h1>;
   }
 }
 
-render(
-  <App/>,
-  document.getElementById('app')
-)
+render(<App />, document.getElementById("app"));
