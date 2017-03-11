8b1b135be6a41a6b49e708ee88f8f23dabb200e2
Sam Clement <sam@swhurl.com>
2017-03-11 17:39:02 +0000

    ADD jest testing w/ enzyme

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+
+export default () => {
+  return <h1>{`Test App`}</h1>
+}
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import App from './components/App.js'
 
-class App extends Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  render() {
-    return <h1>{`Test App 4`}</h1>;
-  }
-}
-
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById("app"))
